c1f6e8b3e2aed8a6ddb2702be0e9e81df0f7deff
Erin Sparling <erin@everyplace.net>
2023-07-23 02:41:47 +0000

    Drafting how a vue app could consume parts of the existing app

---
+++ app/src/components/nav.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+  import comicsList from '../../../js/comics.js'
+
+</script>
+
+<template>
+
+  <nav>
+
+    <ul>
+      <li v-for="comic in comicsList.comics">
+        <a v-bind:href="comic.rssurl">{{ comic.name }}</a>
+      </li>
+    </ul>
+  </nav>
+
+</template>
+
+<style scoped>
+
+</style>
