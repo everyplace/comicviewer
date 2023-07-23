3506c83d1471dd711c8a8dd38f1b0930a84bb4bc
Erin Sparling <erin@everyplace.net>
2023-07-23 03:08:23 +0000

    Working through vue-router

--- app/src/components/nav.vue
+++ app/src/components/nav.vue
@@ -7,12 +7,17 @@
 <template>
 
   <nav>
-
+    <ul>
+      <li><router-link to="/">go to home</router-link></li>
+      <li><router-link to="/test">go to test</router-link></li>
+      <li><router-link to="/one">go to one</router-link></li>
+    </ul>
+<!-- 
     <ul>
       <li v-for="comic in comicsList.comics">
         <a v-bind:href="comic.rssurl">{{ comic.name }}</a>
       </li>
-    </ul>
+    </ul> -->
   </nav>
 
 </template>
--- app/src/main.js
+++ app/src/main.js
@@ -1,5 +1,25 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+const One = { template: '<div>One component...</div>' }
+const Test = { template: '<div>Test component...</div>' }
+
+
+const routes = [
+    { path: '/', component: HelloWorld },
+    { path: '/one', component: One },
+    { path: '/test', component: Test },
+  ]
+
+  const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHashHistory(),
+    routes, // short for `routes: routes`
+  })
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
